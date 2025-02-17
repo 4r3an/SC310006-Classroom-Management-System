5aa776da94942bbe93f06d98370345f1a5c3fdb0
4r3an <ananda.m@kkumail.com>
2025-02-18 00:35:15 +0700

    updates deploy

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'development' ? '/' : '/SC310006-Classroom-Management-System/',
+  base: '/SC310006-Classroom-Management-System/',
   plugins: [
-    react(), tailwindcss()
+    react(),
+    tailwindcss()
   ],
 })
