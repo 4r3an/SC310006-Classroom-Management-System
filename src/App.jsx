959a60622e2bbaf621a0da5b84b6f2264fb17789
TweekyS <115881515+TweekyS@users.noreply.github.com>
2025-02-18 01:40:15 +0700

    เพิ่มClassroom

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import WelcomePage from './components/WelcomePage'
 import LoginPage from './components/LoginPage'
 import DashboardPage from './components/DashboardPage'
+import CreateClassroom from './components/CreateClassroom';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<WelcomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/create-classroom" element={<CreateClassroom />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
